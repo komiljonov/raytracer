eef45a5981251d87955e7a9ab9e7845876e6be6a
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-08-26 13:02:16 +0500

    Implement ray. Implement vec3. Implement image generator

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,65 @@
-
-
+mod ray;
 mod vec3;
 
-
+use ray::Ray;
 use vec3::Vec3;
 
+// fn write_ppm(w: i32, h: i32, max_value: i32) {
+//     println!("P3\n{} {}\n{}", w, h, max_value);
 
+//     for j in (0..h).rev() {
+//         for i in 0..w {
+//             let r = i as f32 / w as f32;
+//             let g = j as f32 / h as f32;
+//             let b: f32 = 0.2;
 
-fn write_ppm(w: i32, h: i32, max_value: i32) {
+//             let ir = (255.99 * r) as i32;
+//             let ig = (255.99 * g) as i32;
+//             let ib = (255.99 * b) as i32;
+
+//             println!("{} {} {}", ir, ig, ib);
+//         }
+//     }
+
+// }
+
+fn color(r: &Ray) -> Vec3 {
+    let unit_direction = Vec3::unit_vector(&r.direction());
+    let t = 0.5 * (unit_direction.y() + 1.0);
+
+    Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
+fn main() {
+    // let width = 200;
+    // let height = 100;
+    // let max_value = 255;
+
+    // we use a plan txt ppm to start building images
+    // write_ppm(width, height, max_value);
+
+    let w = 200;
+    let h = 100;
+    let max_value = 255;
+
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
     println!("P3\n{} {}\n{}", w, h, max_value);
 
     for j in (0..h).rev() {
         for i in 0..w {
-            let r = i as f32 / w as f32;
-            let g = j as f32 / h as f32;
-            let b: f32 = 0.2;
+            let u = i as f32 / w as f32;
+            let v = j as f32 / h as f32;
 
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let r = Ray::ray(origin, lower_left_corner + horizontal * u + vertical * v);
+            let col = color(&r);
+
+            let ir = (255.99 * col.r()) as i32;
+            let ig = (255.99 * col.g()) as i32;
+            let ib = (255.99 * col.b()) as i32;
 
             println!("{} {} {}", ir, ig, ib);
         }
     }
-
-
-}
-
-
-
-fn main() {
-    let width = 200;
-    let height = 100;
-    let max_value = 255;
-
-    // we use a plan txt ppm to start building images
-    write_ppm(width, height, max_value);
-
-
-
-    let v1 = Vec3::new(1f32, 2f32, 6f32);
-    let v2 = Vec3::new(2f32, 3f32, 12f32);
-
-
-    let v3 = v1 + v2;
-
-    println!("Added v1 and v2: {:?}", v3);
-
 }
---
+++ src/ray.rs
@@ -0,0 +1,44 @@
+use crate::vec3::Vec3;
+
+
+#[derive(Debug, Copy,Clone)]
+pub struct Ray {
+    a: Vec3,
+    b: Vec3,
+}
+
+impl Ray {
+    // Constructor method
+    pub fn ray(a: Vec3, b: Vec3) -> Ray {
+        Ray { a, b }
+    }
+
+    // Returns the origin of the ray
+    pub fn origin(self) -> Vec3 {
+        self.a
+    }
+
+    // Returns the direction of the ray
+    pub fn direction(self) -> Vec3 {
+        self.b
+    }
+
+    // Computes the point at a given parameter t
+    pub fn point_at_parameter(self, t: f32) -> Vec3 {
+        self.a + self.b * t
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ray_origin() {}
+
+    #[test]
+    fn test_ray_direction() {}
+
+    #[test]
+    fn test_ray_point_at_parameter() {}
+}
